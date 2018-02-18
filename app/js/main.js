34ced7792dc05a866a8169686eb11a445c3bf2a1
DESKTOP-DD5CSQD\Ruslan <ramag>
2018-02-18 19:43:28 +0200

    layout price-cards and make hover effects

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,12 @@ $(document).ready(function() {
   	nav: true
   });
 
+  // hover effects on button
+  $(".cards-button").hover(function(){
+	$(this).parent().find(".cards-price").css({"background-color" : "#2e43e5"});
+  },function(){
+  	$(this).parent().find(".cards-price").css({"background-color" : "#5b6ceb"});
+  });
+
 });
+// end document.ready 
